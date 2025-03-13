748e1c6a1756db4e9e767749e5a2b46acfe22163
TheBigHummusMan <ethan202005@gmail.com>
2025-03-13 11:00:05 -0400

    changes to sql scripy and server with the db name

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     password: "1234",
     host: "localhost",
     port: 4532,
-    database: "add db name here"
+    database: "CSI_2532_hotel_project"
 });
 
 module.exports = pool;
