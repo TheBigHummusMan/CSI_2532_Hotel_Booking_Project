c2312efc70c4fb3ee89eeefa9b6c254307e3b1ba
TheBigHummusMan <ethan202005@gmail.com>
2025-03-18 14:31:50 -0400

    can now search for rooms with same parameters as hotels
    figured searching for a room is more realistic than finding a hotel

--- server/index.js
+++ server/index.js
@@ -56,7 +56,8 @@ app.get("/address/:ville", async(req, res) => {
     });
 
 // gets all hotels from the db
-app.get("/hotel",async(req, res)=>{
+/* not required because the next get method can return without any parameters
+app.get("/search/hotel",async(req, res)=>{
     try {
         const hotel = await pool.query("SELECT * FROM hotel");
         res.json(hotel.rows);
@@ -64,11 +65,11 @@ app.get("/hotel",async(req, res)=>{
         console.error(err.message);
     }
 });
-
+*/
 // get the hotel based off of the city name
 app.get("/search/hotel", async(req, res)=>{
     try {
-        console.log("Received query parameters:", req.query);
+        console.log("Received query parameters for hotel search:", req.query);
 
         let queryStr = `
             SELECT h.*, a.ville, a.adresseDeRue, c.rating 
@@ -105,12 +106,67 @@ app.get("/search/hotel", async(req, res)=>{
             paramIndex++;
         }
 
-        console.log("Generated SQL Query:", queryStr);
-        console.log("Query Parameters:", params);
+        //debub logs
+        //console.log("Generated SQL Query:", queryStr);
+        //console.log("Query Parameters:", params);
 
         // old query to onlu get based off of city name, const hotel = await pool.query("SELECT h.* FROM hotel h JOIN Address a ON h.addressID = a.addressID WHERE a.ville ILIKE $1", [`%${ville}%`]);
         const hotels = await pool.query(queryStr, params);
         res.json(hotels.rows);
+
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
+// get the room based off of the same criteria but gets all the rooms instead
+app.get("/search/chambre", async(req, res)=>{
+    try {
+        console.log("Received query parameters for rooms:", req.query);
+
+        let queryStr = `
+            SELECT r.*
+            FROM chambre r
+            JOIN hotel h ON h.hotelid = r.hotelid
+            JOIN address a ON h.addressID = a.addressID
+            JOIN chainehoteliere c ON h.nomDeChaine = c.nomDeChaine
+            WHERE 1=1
+        `;
+
+        let {ville, minRating,maxRating,adresseDeRue}=req.query;
+        let params = [];
+        let paramIndex = 1;
+
+        if (ville){
+            queryStr+=` AND a.ville ILIKE $${paramIndex}`;
+            params.push(`%${ville}%`);
+            paramIndex++;
+        }
+        if (minRating){
+            console.log("visited")
+            queryStr += ` AND c.rating >= $${paramIndex}`;
+            params.push(parseFloat(minRating));
+            paramIndex++;
+        }
+        if (maxRating){
+            console.log("visited")
+            queryStr += ` AND c.rating <= $${paramIndex}`;
+            params.push(parseFloat(maxRating));
+            paramIndex++;
+        }
+        if (adresseDeRue){
+            queryStr+=` AND a.adresseDeRue ILIKE $${paramIndex}`;
+            params.push(`%${adresseDeRue}%`);
+            paramIndex++;
+        }
+
+        //debub logs
+        //console.log("Generated SQL Query:", queryStr);
+        //console.log("Query Parameters:", params);
+
+        // old query to onlu get based off of city name, const hotel = await pool.query("SELECT h.* FROM hotel h JOIN Address a ON h.addressID = a.addressID WHERE a.ville ILIKE $1", [`%${ville}%`]);
+        const chambre = await pool.query(queryStr, params);
+        res.json(chambre.rows);
 
     } catch (err) {
         console.error(err.message)
