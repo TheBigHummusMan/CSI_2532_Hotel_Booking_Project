cbb8f8c705bdecdadf6b308ef8774a3a525dfe98
TheBigHummusMan <ethan202005@gmail.com>
2025-03-19 22:31:20 -0400

    cleaned up more code
    started the folders for JWT

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 express.Router
 const cors = require("cors");
-const pool = require("./config/db");
+//const pool = require("./config/db");
 
 // middleware
 app.use(cors());
@@ -12,16 +12,12 @@ app.use(express.json());
 // add the dirrerent actions that a user can do to modify the db, ex add a hotel to the chain, or book a room, etc
 // post add to the db, can use postman to test sending requests to the server without having the frontend ready
 
-const addressRoutes = require("./routes/addressRoutes");
 const hotelRoutes = require("./routes/hotelRoutes");
 const roomRoutes = require("./routes/roomRoutes");
 
 
-app.use("/address", addressRoutes);
 app.use("/", hotelRoutes);
 app.use("/", roomRoutes);
-
-
 
 
 // listens to port 5000 where the server is being locally hosted
--- server/routes/addressRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { addAddress, getAllAddresses, getAddressByCity } = require("../controllers/addressController");
-
-router.post("/", addAddress);
-router.get("/", getAllAddresses);
-router.get("/:ville", getAddressByCity);
-
-module.exports = router;
