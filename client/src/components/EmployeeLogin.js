5bb3abbae3ef0b631351de2ef01436e4368c777b
TheBigHummusMan <ethan202005@gmail.com>
2025-04-02 19:17:35 -0400

    added employe login

--- client/src/components/EmployeeLogin.js
+++ client/src/components/EmployeeLogin.js
@@ -24,7 +24,7 @@ const EmployeeLogin = ({setAuth}) => {
 
             const body = {email, password};
 
-            const response = await fetch('http://localhost:5000/auth/login', {
+            const response = await fetch('http://localhost:5000/login/employe', {
                 method: 'POST', 
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(body)
--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -60,7 +60,7 @@ router.post("/register/client", validInfo, async (req, res) => {
     }
 });
 
-// login route
+// login route for client 
 router.post("/login/client", validInfo,async(req,res)=>{
     try {
         const {email, password} = req.body
@@ -74,6 +74,88 @@ router.post("/login/client", validInfo,async(req,res)=>{
         };
 
         const validPassword = await bcrypt.compare(password, user.rows[0].password);
+
+        if (!validPassword){
+            return res.status(401).json("password or email is incorrect");
+        }
+
+        const token = jwtGenerator(user.rows[0].nas);
+        return res.json({ token });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Server error during registration" });
+    }
+});
+
+// registering for employe
+/*
+router.post("/register/employe", validInfo, async (req, res) => {
+    try {
+        const { nas, nom, password, email, ville, adressederue, codepostal } = req.body;
+
+        // Check if user exists
+        const user = await pool.query("SELECT * FROM employe WHERE employeeid = $1", [nas]);
+
+        if (user.rows.length !== 0) {
+            return res.status(401).json({ error: "User already exists" });
+        }
+
+        // Encrypt password
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+        const bcryptPassword = await bcrypt.hash(password, salt);
+
+        let addressId;
+        // Check if address exists
+        const address = await pool.query(
+            "SELECT addressid FROM address WHERE ville = $1 AND adressederue = $2 AND codepostal = $3",
+            [ville, adressederue, codepostal]
+        );
+
+        if (address.rows.length !== 0) {
+            addressId = address.rows[0].addressid;
+        } else {
+            // Create new address
+            const newAddress = await pool.query(
+                "INSERT INTO address (ville, adressederue, codepostal) VALUES($1, $2, $3) RETURNING addressid",
+                [ville, adressederue, codepostal]
+            );
+            addressId = newAddress.rows[0].addressid;
+        }
+
+        // Create new user
+        const newUser = await pool.query(
+            "INSERT INTO employe (employeeid, nom, addressid, password, email) VALUES($1, $2, $3, $4, $5) RETURNING *",
+            [employeeid, nom, addressId, bcryptPassword, email]
+        );
+
+        
+
+        // Generate token
+        const token = jwtGenerator(newUser.rows[0].nas);
+        res.json({ token });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Server error during registration" });
+    }
+});*/
+
+// login route for employe
+router.post("/login/employe", validInfo,async(req,res)=>{
+    try {
+        const {email, password} = req.body
+
+        const user = await pool.query("SELECT * FROM employe  WHERE email = $1",
+            [email]
+        );
+
+        if (user.rows.length == 0){
+            return res.status(401).json("user doesn't exist");
+        };
+
+        const validPassword = (password == user.rows[0].password);
 
         if (!validPassword){
             return res.status(401).json("password or email is incorrect");
